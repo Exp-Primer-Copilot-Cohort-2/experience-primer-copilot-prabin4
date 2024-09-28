ffba8b3aeb3c40241d71a38e66f061669688ff65
Prabin Lamichhane <67856931+prabin4@users.noreply.github.com>
2024-09-28 18:07:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,51 +2,37 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var qs = require('querystring');
-var path = require('path');
+var port = 3000;
 
 //create server
-http.createServer(function(req, res) {
-    var url_parts = url.parse(req.url, true);
-    var query = url_parts.query;
+http.createServer(function (req, res) {
 
-    //if request is post
-    if (req.method == 'POST') {
-        var body = '';
-        req.on('data', function(data) {
-            body += data;
-            //if body > 1e6
-            if (body.length > 1e6) {
-                req.connection.destroy();
-            }
-        });
-        req.on('end', function() {
-            var POST = qs.parse(body);
-            var comment = POST['comment'];
-            if (comment) {
-                fs.appendFile('comments.txt', comment + '\n', function(err) {
-                    if (err) {
-                        console.log(err);
-                    }
-                });
-            }
-        });
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.end();
+  //parse the request containing file name
+  var pathname = url.parse(req.url).pathname;
+
+  //print the name of the file for which request is made
+  console.log("Request for " + pathname + " received.");
+
+  //read the requested file content from file system
+  fs.readFile(pathname.substr(1), function (err, data) {
+    if (err) {
+      console.log(err);
+      //HTTP Status: 404: NOT FOUND
+      //Content Type: text/plain
+      res.writeHead(404, { 'Content-Type': 'text/html' });
     } else {
-        //if request is get
-        fs.readFile('comments.txt', function(err, data) {
-            if (err) {
-                console.log(err);
-            }
-            res.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            res.write(data);
-            res.end();
-        });
+      //Page found
+      //HTTP Status: 200 : OK
+      //Content Type: text/plain
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+
+      //write the content of the file to response body
+      res.write(data.toString());
     }
-}).listen(8080);
-console.log('Server running at http://localhost:8080');
+    //send the response body
+    res.end();
+  });
+}).listen(port);
+
+//print the message
+console.log("Server running at http://localhost:" + port + "/");
