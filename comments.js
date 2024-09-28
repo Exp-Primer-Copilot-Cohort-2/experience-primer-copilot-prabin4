e3b264bbe458a51483833ead1fb55f1130331746
Prabin Lamichhane <67856931+prabin4@users.noreply.github.com>
2024-09-28 18:14:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,13 @@
-//create web server
+// Create web server
+const express = require('express');
 const app = express();
-const port = 3000;
-const path = require('path');
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const comments = require('./comments.json');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/comments', (req, res) => {
-  res.json(comments);
+// Create a route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-app.post('/comments', (req, res) => {
-  const newComment = req.body;
-  comments.push(newComment);
-
-  fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
-    if (err) {
-      console.log("Error writing file:", err);
-    }
-  });
-
-  res.json(newComment);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Start server
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
 });
