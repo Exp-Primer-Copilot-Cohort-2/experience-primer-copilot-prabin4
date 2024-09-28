dc8fa845fdfa8c2b614ede7c50b8a0fb9ac5ba8a
Prabin Lamichhane <67856931+prabin4@users.noreply.github.com>
2024-09-28 18:09:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,38 +1,34 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var port = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const comments = require('./comments.json');
 
-//create server
-http.createServer(function (req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  //parse the request containing file name
-  var pathname = url.parse(req.url).pathname;
+app.use(express.static(path.join(__dirname, 'public')));
 
-  //print the name of the file for which request is made
-  console.log("Request for " + pathname + " received.");
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
 
-  //read the requested file content from file system
-  fs.readFile(pathname.substr(1), function (err, data) {
+app.post('/comments', (req, res) => {
+  const newComment = req.body;
+  comments.push(newComment);
+
+  fs.writeFile('./comments.json', JSON.stringify(comments), (err) => {
     if (err) {
-      console.log(err);
-      //HTTP Status: 404: NOT FOUND
-      //Content Type: text/plain
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-    } else {
-      //Page found
-      //HTTP Status: 200 : OK
-      //Content Type: text/plain
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-
-      //write the content of the file to response body
-      res.write(data.toString());
+      console.log("Error writing file:", err);
     }
-    //send the response body
-    res.end();
   });
-}).listen(port);
 
-//print the message
-console.log("Server running at http://localhost:" + port + "/");
+  res.json(newComment);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
