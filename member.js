ab293a64605c709e8ce40a8880f149fdbf4d7a22
Prabin Lamichhane <67856931+prabin4@users.noreply.github.com>
2024-09-28 17:52:01 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillMember() {
+    var member = {
+        name: 'Budi',
+        age: 20,
+        address: 'Jakarta',
+        hobby: ['Membaca', 'Olahraga', 'Menulis']
+    }
+
+    return member;
+}
